a794cf8c9bf4f9e89ad4fcff63e05d7487fbe80c
jfmartinezIBEC <54336211+jfmartinezIBEC@users.noreply.github.com>
2019-08-22 15:53:49 -0300

    Código de muestra
    cambio de color

---
+++ clase05.js
@@ -0,0 +1,5 @@
+// Introducción al DOM 
+
+// Ejemplo #1
+document.body.style.background = 'red'; // la propiedad background de el tag body es rojo
+setTimeout(() => document.body.style.background = '', 5000); // la propiedad background de el tag body cambia a los 5 seg
